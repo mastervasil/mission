5fd45af4130ed450dbccca5877e7f1756efe4e5a
masterphantom <masterphantom@bk.ru>
2015-11-22 01:06:36 +0300

    add test

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,4 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
 
@@ -12,7 +11,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -25,4 +26,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from polls.models import Question
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertFalse(future_question.was_published_recently())
